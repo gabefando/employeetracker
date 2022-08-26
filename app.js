2387c930f47ef1e0166a8de2b1c0211735e82771
gabefando <gabe.fonseca.04@gmail.com>
2022-08-26 11:24:39 -0400

    configured schema to create tables and connected on app.js

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const mysql = require('mysql2')
 const inquirer = require('inquirer')
 const cTable = require('console.table');
 
+const db = mysql.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      password: "mynewpassword",
+      database: "company_db",
+    },
+  );
